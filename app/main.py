521acec8383993f5948d575b94522c8822effe1b
lwyso <lwysocki91@gmail.com>
2026-05-14 23:29:40 +0200

    add validation

--- app/main.py
+++ app/main.py
@@ -1,29 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
+from typing import Annotated
 from contextlib import asynccontextmanager
+from pydantic import EmailStr
 from sqlmodel import Session
 
-from app.services.auth import hash_password
+from app.services.auth import hash_password, validate_password
 
 from .database import create_db_and_tables, engine
 from .models.user import User
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-  create_db_and_tables()
-  yield
+    create_db_and_tables()
+    yield
+
 
 app = FastAPI(lifespan=lifespan)
 
+
 @app.get("/health")
 async def health():
-  return {"status": "healthy"}
+    return {"status": "healthy"}
+
 
 @app.post("/users")
-async def createUser(email: str, password: str, user_name: str):
-  password_hash = hash_password(password)
+async def createUser(
+    email: EmailStr,
+    password: Annotated[str, Depends(validate_password)],
+    user_name: Annotated[str, Query(min_length=4)],
+):
+    password_hash = hash_password(password)
+    user = User(email=email, password_hash=password_hash, user_name=user_name)
 
-  user = User(email=email, password_hash=password_hash, user_name=user_name)
-
-  with Session(engine) as session:
-    session.add(user)
-    session.commit()
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
--- app/services/auth.py
+++ app/services/auth.py
@@ -1,8 +1,22 @@
 import bcrypt
+import re
+from typing import Annotated
+from fastapi import Query, HTTPException
+
 
 def hash_password(password: str):
-  password_bytes = password.encode("utf-8")
-  salt = bcrypt.gensalt()
-  password_hash = bcrypt.hashpw(password_bytes, salt)
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    password_hash = bcrypt.hashpw(password_bytes, salt)
 
-  return password_hash
+    return password_hash
+
+
+def validate_password(password: Annotated[str, Query(min_length=8)]):
+    if not (re.search(r"[A-Z]", password)):
+        raise HTTPException(422, "Password must contain an uppercase letter")
+    if not (re.search(r"\d", password)):
+        raise HTTPException(422, "Password must contain a digit")
+    if not re.search(r"[^a-zA-Z0-9]", password):
+        raise HTTPException(422, "Password must contain a special character")
+    return password
