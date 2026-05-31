ef09928fb853c759ce8502e24d3abde28bb5fae7
lwyso <lwysocki91@gmail.com>
2026-05-31 14:49:17 +0200

    scan receipt

---
+++ app/exceptions.py
@@ -0,0 +1,8 @@
+class OcrError(Exception):
+    def __init__(self, message="Cannot read an image"):
+        super().__init__(message)
+
+
+class ParsingError(Exception):
+    def __init__(self, message="Cannot parse the receipt"):
+        super().__init__(message)
