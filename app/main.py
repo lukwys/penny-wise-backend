cbe9f52cb5f7e3793264cf5178a8c8f438ddb7e1
lwyso <lwysocki91@gmail.com>
2026-09-07 20:17:26 +0200

    auth router

--- app/main.py
+++ app/main.py
@@ -5,25 +5,18 @@ from app.models.expense import Expense
 from app.schemas.expense import ExpenseCreate, ExpenseRead
 from app.services.ai_parser import get_parser
 from app.services.receipt_scanner import scan_receipt_text
-from app.schemas.token import Token
-from fastapi import FastAPI, HTTPException, Query, Depends, Request, UploadFile
+from fastapi import FastAPI, Depends, Request, UploadFile
 from typing import Annotated
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordRequestForm
-from pydantic import EmailStr
 from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
-import bcrypt
 
 from app.services.auth import (
-    create_access_token,
-    hash_password,
-    validate_password,
     validate_token,
 )
 
 from .database import engine
-from .models.user import User
+from .routers.auth import auth_router
 
 app = FastAPI()
 
@@ -32,6 +25,8 @@ CONSTRAINT_MESSAGES = {
     "user_email_key": "Email already used",
     "fallback": "Database constraint violated",
 }
+
+app.include_router(auth_router)
 
 
 @app.exception_handler(IntegrityError)
@@ -68,42 +63,6 @@ def parsing_exception_handler(_request, exc: ParsingError):
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
-
-
-@app.post("/users")
-async def create_user(
-    email: EmailStr,
-    password: Annotated[str, Depends(validate_password)],
-    user_name: Annotated[str, Query(min_length=4)],
-):
-    password_hash = hash_password(password)
-    user = User(email=email, password_hash=password_hash, user_name=user_name)
-
-    with Session(engine) as session:
-        session.add(user)
-        session.commit()
-
-
-@app.post("/sessions")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    with Session(engine) as session:
-        statement = select(User).where(form_data.username == User.email)
-        user = session.exec(statement).first()
-
-        if user is None:
-            raise HTTPException(status_code=401, detail="Invalid credentials")
-
-        verify_password = bcrypt.checkpw(
-            form_data.password.encode(), user.password_hash.encode()
-        )
-
-        if not verify_password:
-            raise HTTPException(status_code=401, detail="Invalid credentials")
-
-        access_token = create_access_token({"sub": str(user.id)})
-
-        return Token(access_token=access_token, token_type="bearer")
-
 
 @app.post("/expenses/scan", dependencies=[Depends(validate_token)])
 async def scan_receipt(receipt: UploadFile):
